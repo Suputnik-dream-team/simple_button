3fefcd5132e8357a300695d6cb13465c3eda2790
Peter <47149688+mccolaichuk@users.noreply.github.com>
2021-03-15 19:20:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-alert('Вікно має вилізти');
-var myButton = document.querySelector('button');
-
-function selectFiles(){
-
-}
-
-myButton.onclick = function() {
-  selectFiles();
+function updateSize() {
+  var nBytes = 0,
+      oFiles = document.getElementById("uploadInput").files,
+      nFiles = oFiles.length;
+  for (var nFileId = 0; nFileId < nFiles; nFileId++) {
+    nBytes += oFiles[nFileId].size;
+  }
+  var sOutput = nBytes + " bytes";
+  // optional code for multiples approximation
+  for (var aMultiples = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"], nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++) {
+    sOutput = nApprox.toFixed(3) + " " + aMultiples[nMultiple] + " (" + nBytes + " bytes)";
+  }
+  // end of optional code
+  document.getElementById("fileNum").innerHTML = nFiles;
+  document.getElementById("fileSize").innerHTML = sOutput;
 }
